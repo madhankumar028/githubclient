8801468e693cc7d33a531c41a2f8993de678dd00
madhankumar028 <madhankumar029@outlook.com>
2016-11-28 22:21:32 +0530

    initial commit

---
+++ client/scripts/about/aboutController.js
@@ -0,0 +1,12 @@
+(function () {
+
+    'use strict';
+
+    angular
+    .module('app.about', [])
+    .controller('AboutController', AboutCtrl);
+
+    function AboutCtrl() {
+
+    }
+})();
---
+++ client/scripts/app.module.js
@@ -0,0 +1,47 @@
+(function () {
+
+    'use strict';
+
+    /**
+    * Currently manages all aspects of the application.This will be refactored into
+    * multiple modules later on.
+    *
+    * @module app
+    */
+    angular.module('app', [
+
+                    'ui.router',
+
+                    /* Features */
+                    'app.constant',
+                    'app.home',
+                    'app.about'
+                    ])
+
+    /**
+    * Default configuration for the application.
+    *
+    *
+    * @memberof module:app
+    *
+    * @requires $urlRouterProvider
+    * @requires $stateProvider
+    *
+    */
+    .config(function ($urlRouterProvider, $stateProvider) {
+
+        $stateProvider.state('home', {
+            name: 'home',
+            url: '/home',
+            templateUrl: 'views/home.html',
+            controller: 'HomeController as HomeCtrl'
+        }).state('about', {
+            name: 'about',
+            url: '/about',
+            templateUrl: 'views/about.html',
+            controller: 'AboutController as AboutCtrl'
+        });
+
+        $urlRouterProvider.otherwise('/home');
+    });
+})();
