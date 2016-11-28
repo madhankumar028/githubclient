0b67e91929372f6d08e9a4ea4c7e5c6bc4a20966
madhankumar028 <madhankumar029@outlook.com>
2016-11-28 22:32:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var fs    = require('fs'),
+    http  = require('http');
+
+http.createServer(function (request, response) {
+    fs.createReadStream('${__dirname}/index.html'}.pipe(response);
+}).listen(8000);
+
+console.log('Listening on port 8000.');
