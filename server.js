b12437599ee9a8b66b3cc4f6601221e2892b476f
madhankumar028 <madhankumar029@outlook.com>
2016-12-12 02:52:02 +0530

    test

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var app = express();
+var fs    = require('fs'),
+    http  = require('http');
 
-app.use(express.static(__dirname + '/index.html'));
+http.createServer(function (request, response) {
+    fs.createReadStream('${__dirname}/index.html'}.pipe(response);
+}).listen(8000 || process.env.PORT);
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log('Listening at port ' + port);
-});
+console.log('Listening on port 8000.');
+
+// var express = require('express');
+// var app = express();
+//
+// app.use(express.static(__dirname + '/index.html'));
+//
+// var server = app.listen(process.env.port || 80, function () {
+//   var port = server.address().port;
+//   console.log('Listening at port ' + port);
+// });
