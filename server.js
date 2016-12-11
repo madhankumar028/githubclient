6d0a73e0499f1761c3d45aaa2eddd75f78185eb2
madhankumar028 <madhankumar029@outlook.com>
2016-12-12 00:27:39 +0530

    added node_modules

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var fs    = require('fs'),
 
 http.createServer(function (request, response) {
     fs.createReadStream('${__dirname}/index.html'}.pipe(response);
-}).listen(8000);
+}).listen(8000 || process.env.port);
 
 console.log('Listening on port 8000.');
