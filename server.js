d359f88a9a486445b1bd70af5c660c13ef8a375b
madhankumar028 <madhankumar029@outlook.com>
2016-12-12 03:04:31 +0530

    added get request

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 var server = app.listen(process.env.PORT || 80, function () {
   var port = server.address().port;
