e5a8b3f9ef00ed8b55ca0b41234723aaf370e7f7
madhankumar <madhankumar028@gmail.com>
2016-12-14 12:21:46 +0530

    fixed paranthesis

--- client/scripts/app.module.js
+++ client/scripts/app.module.js
@@ -43,4 +43,4 @@
 
         $urlRouterProvider.otherwise('/home');
     });
-})();
+}());
--- client/scripts/home/homeService.js
+++ client/scripts/home/homeService.js
@@ -1,4 +1,4 @@
-  (function () {
+(function () {
 
     'use strict';
 
@@ -69,4 +69,4 @@
             return deferred.promise;
         }
     }
-})();
+}());
