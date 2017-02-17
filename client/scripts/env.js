3e603c1e919a19ceecfc8488a0b94608012cf98c
madhankumar028@gmail.com <madhankumar028@gmail.com>
2017-02-17 02:55:35 -0500

    added environment variables

---
+++ client/scripts/env.js
@@ -0,0 +1,23 @@
+(function (window) {
+    
+    window.__env = window.__env || {};
+
+    // API url
+    window.__env.apiUrl = 'https://api.github.com/users/';
+
+    // Base url
+    window.__env.baseUrl = 'https://api.github.com/users';
+
+    // Default user
+    window.__env.defaultUser = 'madhankumar028';
+
+    // Repo count
+    window.__env.repoCount = 10;
+
+    // ClientID
+    window.__env.clientId = 'b7641fc061fbc7eba0ae';
+
+    // ClientSecret
+    window.__env.clientSecret = '582f452b977885775b36fd81d8bfe51a5d48e59d';
+
+}(this));
